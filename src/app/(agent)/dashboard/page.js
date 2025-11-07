4e0dbbade51f907094c3b93aa1fbdc9f7567de35
sal-digtan <salman.digtan@gmail.com>
2025-11-07 15:21:06 +0500

    minor updates

--- src/app/(agent)/dashboard/page.js
+++ src/app/(agent)/dashboard/page.js
@@ -4,7 +4,7 @@ import React, { useState } from 'react'
 import AgentDashboard from '@/app/components/agent_dashboard';
 import { LocationContext } from '@/app/context/LocationContext';
 
-const page = () => {
+const Page = () => {
 
     const [locationSearch, setLocationSearch] = useState('3911 Firestone Blvd, South Gate, CA 90280, United States')
 
@@ -17,4 +17,4 @@ const page = () => {
     )
 }
 
-export default page
+export default Page
